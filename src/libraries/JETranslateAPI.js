ee81bcdb817088e889708a0ceeb5d384cb15e983
ENEIZEM <pomazuevmaksim@gmail.com>
2024-02-05 21:27:11 +0300

    work on a library for simplified translation of global mods into other languages continues

--- src/libraries/JETranslateAPI.js
+++ src/libraries/JETranslateAPI.js
@@ -52,26 +52,38 @@ let SimpleTranslation = {
     }(),
 
     getPathsForEachlanguage: function(path){
-        //проверяем, являются ли файлы в дирректории файлами перевода, достаточно наличие файла en.txt/en_us.txt/en_US.txt/en.lang/en_us.lang/en_US.lang
+        //создаём перемнную для массива путей к файлам, находящимся в папке path
         let pathsToFiles = FileTools.GetListOfFiles(path);
+        //создаём перемнную для массива путей к директориям, находящимся в папке path
         let pathsToDirs = FileTools.GetListOfDirs(path);
+        //создаём перемнную для объединения массивов путей к файлам и директориям, находящимся в папке path
         let pathsToFilesAndDirs = pathsToFiles.concat(pathsToDirs)
+        //создаём цикл с индексом indexOfPath. При каждой итерации цикла будут перебираться все файлы и директории из массива путей к файлам и директориям, находящимся в папке path
         for(let indexOfPath = 0; indexOfPath < pathsToFilesAndDirs.length - 1; indexOfPath++){
+            //создаём перемнную, хранящую название текущего файла или директории из массива путей к файлам и директориям, находящимся в папке path
+            let fileOrDirName = pathsToFilesAndDirs[indexOfPath].getName()
+            //создаём цикл с индексом indexOfKey. При каждой итерации цикла будут перебираться все языковые объекты ключей объекта SimpleTranslation.language
             for(let indexOfKey = 0; indexOfKey < 30; indexOfKey++){
-                let nameOfKey = Object.keys(this.language)[indexOfKey];
-                function setPathsForEachlanguage(arrayOfPaths){        
-                    let arrayOfNextLevelPaths;
-                    arrayOfPaths.forEach(function(element){
-                        if (element.isFile()){
-                            Object.assign(this.language[nameOfKey], element.ReadKeyValueFile(element.getPath(), "="))
-                        } else if (element.isDirectory()){
-                            arrayOfNextLevelPaths = FileTools.GetListOfDirs(element.getPath())
+                //создаём перемнную, хранящую название текущего языкового ключа объекта SimpleTranslation.language
+                let nameOfKey = Object.keys(SimpleTranslation.language)[indexOfKey];  
+                //создаём перемнную для массива возможных названий файла или директории из массива путей к файлам и директориям, находящимся в папке path, на основе названия текущего языкового ключа объекта SimpleTranslation.language
+                let arrayOfKey = [nameOfKey, nameOfKey.slice(0, 2), nameOfKey.slice(0, 3) + nameOfKey.slice(3, 5).toUpperCase()];
+                //проверка того, что название текущего файла или директории из массива путей к файлам и директориям, находящимся в папке path, соответствует одному из возможных названий файла или директории
+                if(!!~arrayOfKey.indexOf(fileOrDirName.slice(0, fileOrDirName.lastIndexOf(".") != -1 ? fileOrDirName.lastIndexOf(".") : fileOrDirName.length))){
+                    function setPathsForEachlanguage(arrayOfPaths){        
+                        let arrayOfNextLevelPaths;
+                        arrayOfPaths.forEach(function(element){
+                            if (element.isFile()){
+                                Object.assign(SimpleTranslation.language[nameOfKey], element.ReadKeyValueFile(element.getPath(), "="))
+                            } else if (element.isDirectory()){
+                                arrayOfNextLevelPaths = FileTools.GetListOfDirs(element.getPath())
+                            }
+                        });
+                        if (!arrayOfNextLevelPaths){
+                            setPathsForEachlanguage(arrayOfNextLevelPaths)
                         }
-                    });
-                    if (!arrayOfNextLevelPaths){
-                        setPathsForEachlanguage(arrayOfNextLevelPaths)
-                    }
-                }(pathsToFilesAndDirs);
+                    }(pathsToFilesAndDirs);
+                }
             }
         }
     }(SimpleTranslation.pathToTranslateDir),
@@ -80,37 +92,41 @@ let SimpleTranslation = {
         function getLanguageKey(languageKey){
             return SimpleTranslation.language[languageKey][key]
         };
-        Translation.translate(key);
-        Translation.addTranslation(key, {
+        let translationObject = {
             "en_us": !!getLanguageKey("en_us") ? getLanguageKey("en_us") : key,
-            "bg_bg": !!getLanguageKey("bg_bg") ? getLanguageKey("bg_bg") : this["en_us"],
-            "cs_cz": ,
-            "da_dk": ,
-            "de_de": ,
-            "el_gr": ,
-            "en_gb": ,
-            "es_es": ,
-            "es_mx": ,
-            "fi_fi": ,
-            "fr_ac": ,
-            "fr_fr": ,
-            "hu_hu": ,
-            "id_id": ,
-            "it_it": ,
-            "ja_jp": ,
-            "ko_kr": ,
-            "nb_no": ,
-            "no_no": ,
-            "nl_nl": ,
-            "pl_pl": ,
-            "pt_br": ,
-            "pt_pt": ,
-            "ru_ru": ,
-            "sk_sk": ,
-            "tr_tr": ,
-            "uk_ua": ,
-            "zh_cn": ,
-            "zh_tw": ,
-        })
+            "bg_bg": !!getLanguageKey("bg_bg") ? getLanguageKey("bg_bg") : translationObject["en_us"],
+            "cs_cz": !!getLanguageKey("cs_cz") ? getLanguageKey("cs_cz") : translationObject["en_us"],
+            "da_dk": !!getLanguageKey("da_dk") ? getLanguageKey("da_dk") : translationObject["en_us"],
+            "de_de": !!getLanguageKey("de_de") ? getLanguageKey("de_de") : translationObject["en_us"],
+            "el_gr": !!getLanguageKey("el_gr") ? getLanguageKey("el_gr") : translationObject["en_us"],
+            "en_gb": !!getLanguageKey("en_gb") ? getLanguageKey("en_gb") : translationObject["en_us"],
+            "es_es": !!getLanguageKey("es_es") ? getLanguageKey("es_es") : translationObject["en_us"],
+            "es_mx": !!getLanguageKey("es_mx") ? getLanguageKey("es_mx") : translationObject["es_es"],
+            "fi_fi": !!getLanguageKey("fi_fi") ? getLanguageKey("fi_fi") : translationObject["en_us"],
+            //fr_fr и fr_ac поменяны местами
+            "fr_fr": !!getLanguageKey("fr_fr") ? getLanguageKey("fr_fr") : translationObject["en_us"],
+            "fr_ac": !!getLanguageKey("fr_ac") ? getLanguageKey("fr_ac") : translationObject["fr_fr"],
+            "hu_hu": !!getLanguageKey("hu_hu") ? getLanguageKey("hu_hu") : translationObject["en_us"],
+            "id_id": !!getLanguageKey("id_id") ? getLanguageKey("id_id") : translationObject["en_us"],
+            "it_it": !!getLanguageKey("it_it") ? getLanguageKey("it_it") : translationObject["en_us"],
+            "ja_jp": !!getLanguageKey("ja_jp") ? getLanguageKey("ja_jp") : translationObject["en_us"],
+            "ko_kr": !!getLanguageKey("ko_kr") ? getLanguageKey("ko_kr") : translationObject["en_us"],
+            //no_no и nb_no поменяны местами
+            "no_no": !!getLanguageKey("no_no") ? getLanguageKey("no_no") : translationObject["en_us"],
+            "nb_no": !!getLanguageKey("nb_no") ? getLanguageKey("nb_no") : translationObject["no_no"],
+            "nl_nl": !!getLanguageKey("nl_nl") ? getLanguageKey("nl_nl") : translationObject["en_us"],
+            "pl_pl": !!getLanguageKey("pl_pl") ? getLanguageKey("pl_pl") : translationObject["en_us"],
+            //pt_pt и pt_br поменяны местами
+            "pt_pt": !!getLanguageKey("pt_pt") ? getLanguageKey("pt_pt") : translationObject["en_us"],
+            "pt_br": !!getLanguageKey("pt_br") ? getLanguageKey("pt_br") : translationObject["pt_pt"],
+            "ru_ru": !!getLanguageKey("ru_ru") ? getLanguageKey("ru_ru") : translationObject["en_us"],
+            "sk_sk": !!getLanguageKey("sk_sk") ? getLanguageKey("sk_sk") : translationObject["en_us"],
+            "tr_tr": !!getLanguageKey("tr_tr") ? getLanguageKey("tr_tr") : translationObject["en_us"],
+            "uk_ua": !!getLanguageKey("uk_ua") ? getLanguageKey("uk_ua") : translationObject["en_us"],
+            "zh_cn": !!getLanguageKey("zh_cn") ? getLanguageKey("zh_cn") : translationObject["en_us"],
+            "zh_tw": !!getLanguageKey("zh_tw") ? getLanguageKey("zh_tw") : translationObject["zh_cn"],
+        };
+        Translation.translate(key);
+        Translation.addTranslation(key, translationObject)
     }
 };
